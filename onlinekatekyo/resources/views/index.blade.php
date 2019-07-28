f8b82512b9771f6bcb1a18610a3fde0518987a61
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-07-28 11:17:00 +0900

    htmlファイル追加

---
+++ onlinekatekyo/resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <title>オンスタ</title>
+
+    <link rel="stylesheet" href="css/stylesheet.css">
+
+</head>
+
+<body>
+<div class="header">
+    <div class="header-logo">
+        <div class="header-logo1">オンライン家庭教師の</div>
+        <div class="header-logo2">オンスタ</div>
+    </div>
+    <div class="header-list">
+        <ul>
+            <a href="/">ホーム</a>
+            <a href="/about">オンライン家庭教師とは？？</a>
+            <a href="/charm">オンスタの魅力</a>
+            <a href="/list">講師一覧</a>
+            <a href="/price">お申込み・お問い合わせはこちら</a>
+        </ul>
+    </div>
+
+</div>
+<div class="main">
+
+</div>
+<div class="footer">
+  <div class="footer-logo">
+    <div class="footer-logo1">オンライン家庭教師の</div>
+    <div class="footer-logo2">オンスタ</div>
+  </div>
+  <div class="footer-list">
+     <ul>
+    　<li>会社概要</li>
+      <li>お問い合わせ</li>
+     </ul>
+  </div>
+　　　　
+</div>
+</body>
+</html>
