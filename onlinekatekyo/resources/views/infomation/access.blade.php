cd13058ecce59eedd2a846ca3338c1d2978754d9
kuma <e165720@ie.u-ryukyu.ac.jp>
2019-08-23 14:43:44 +0800

    webテンプレートのhtml,js,cssファイルを組み込んだ
    ルーティング、コントローラ、htmlの拡張子を変えた。

---
+++ onlinekatekyo/resources/views/infomation/access.blade.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>老人ホーム・介護施設・デイサービス向け 無料ホームページテンプレート tp_home4</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ここにサイト説明を入れます">
+<meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/slide.css">
+<link rel="alternate stylesheet" href="css/change.css" title="change">
+<script src="js/openclose.js"></script>
+<script src="js/fixmenu_pagetop.js"></script>
+<script src="js/styleswitcher.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<div id="container">
+
+<header>
+<h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="サンプルホーム"></a></h1>
+<ul id="headermenu">
+<li><a href="contact.html" class="btn1">資料請求・お問い合わせ</a></li>
+<li><a href="contact2.html" class="btn1">見学のお申し込み</a></li>
+</ul>
+<!--文字サイズ変更ボタン-->
+<div id="fsize">
+<p>文字サイズ</p>
+<ul>
+<li id="small"><a href="#" onclick="setActiveStyleSheet('default'); return false;" title="文字サイズ「小」"></a></li>
+<li id="large"><a href="#" onclick="setActiveStyleSheet('change'); return false;" title="文字サイズ「大」"></a></li>
+</ul>
+</div>
+</header>
+
+<aside id="subimg">
+<img src="images/subimg.jpg" alt="">
+</aside>
+
+<!--PC用（801px以上端末）メニュー-->
+<nav id="menubar">
+<ul>
+<li><a href="index.html">ホーム<span>Home</span></a></li>
+<li><a href="info.html">施設のご案内<span>Information</span></a></li>
+<li><a href="service.html">サービスのご案内<span>Service</span></a></li>
+<li><a href="charge.html">ご利用料金<span>Charge</span></a></li>
+<li class="current"><a href="access.html">アクセス<span>Access</span></a></li>
+<li><a href="faq.html">よく頂く質問<span>Faq</span></a></li>
+</ul>
+</nav>
+
+<!--小さな端末用（800px以下端末）メニュー-->
+<nav id="menubar-s">
+<ul>
+<li><a href="index.html">ホーム<span>Home</span></a></li>
+<li><a href="info.html">施設のご案内<span>Information</span></a></li>
+<li><a href="service.html">サービスのご案内<span>Service</span></a></li>
+<li><a href="charge.html">ご利用料金<span>Charge</span></a></li>
+<li class="current"><a href="access.html">アクセス<span>Access</span></a></li>
+<li><a href="faq.html">よく頂く質問<span>Faq</span></a></li>
+</ul>
+</nav>
+
+<div id="contents">
+
+<section>
+
+<h2>アクセス・周辺環境<span>Access</span></h2>
+
+<table class="ta1">
+<caption>アクセス</caption>
+<tr>
+<th>所在地</th>
+<td>東京都XX区XXXX</td>
+</tr>
+<tr>
+<th>地図</th>
+<td><a href="http://template-party.com/file/pickup_googlemap.html">Google Mapを使いたい人はこちらの解説をご覧下さい。</a><br>
+<img src="images/dummy_map_main.jpg" alt=""></td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明を入れます。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明を入れます。</td>
+</tr>
+<tr>
+<th>見出し</th>
+<td>ここに説明を入れます。</td>
+</tr>
+</table>
+
+<table class="ta1">
+<caption>周辺環境</caption>
+<tr>
+<th>医療機関</th>
+<td>ここに説明を入れます。</td>
+</tr>
+<tr>
+<th>商店街</th>
+<td>ここに説明を入れます。</td>
+</tr>
+<tr>
+<th>公園</th>
+<td>ここに説明を入れます。</td>
+</tr>
+</table>
+
+</section>
+
+</div>
+<!--/#contents-->
+
+</div>
+<!--/#container-->
+
+<footer>
+
+<div id="footermenu">
+<ul>
+<li><a href="index.html">ホーム</a></li>
+<li><a href="company.html">会社概要</a></li>
+<li><a href="info.html">施設のご案内</a></li>
+<li><a href="service.html">サービスのご案内</a></li>
+<li><a href="charge.html">ご利用料金</a></li>
+<li><a href="access.html">アクセス</a></li>
+<li><a href="faq.html">よく頂く質問</a></li>
+<li><a href="recruit.html">採用情報</a></li>
+</ul>
+<ul>
+<li class="title"><a href="#">メニューのタイトル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+<ul>
+<li class="title"><a href="#">メニューのタイトル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+<ul>
+<li class="title"><a href="#">メニューのタイトル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+</div>
+<!--/footermenu-->
+
+<div id="copyright">
+<small>Copyright&copy; <a href="index.html">サンプルホーム</a> All Rights Reserved.</small>
+<span class="pr"><a href="http://template-party.com/" target="_blank">《Web Design:Template-Party》</a></span>
+</div>
+
+</footer>
+
+<p class="nav-fix-pos-pagetop"><a href="#">↑</a></p>
+
+<!--メニュー開閉ボタン-->
+<div id="menubar_hdr" class="close"></div>
+<!--メニューの開閉処理条件設定　800px以下-->
+<script>
+if (OCwindowWidth() <= 800) {
+	open_close("menubar_hdr", "menubar-s");
+}
+</script>
+
+</body>
+</html>
