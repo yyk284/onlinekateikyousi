01eedb9a4a0de77c1395647f68ba70fd41beb917
RyumaRyama <e165730@ie.u-ryukyu.ac.jp>
2019-09-10 22:47:30 +0900

    modalのテスト

--- onlinekatekyo/resources/views/tutor/index.blade.php
+++ onlinekatekyo/resources/views/tutor/index.blade.php
@@ -4,44 +4,44 @@
 <div id="contents">
 
 <article>
+    <h2>講師紹介</h2>
 
-<h2>講師紹介</h2>
-<p class="c">必要に応じて各詳細ページを作って下さい。</p>
+    <!-- 切り替えボタンの設定 -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">
+     ココを押すと表示
+    </button>
 
-<figure class="mb30"><img src="/images/sample1.jpg" alt="写真の説明を入れます"></figure>
-
-<table class="ta1">
-<caption>詳細情報</caption>
-<tr>
-<th>サンプル見出し</th>
-<td>サンプルテキスト。</td>
-</tr>
-<tr>
-<th>サンプル見出し</th>
-<td>サンプルテキスト。</td>
-</tr>
-<tr>
-<th>サンプル見出し</th>
-<td>サンプルテキスト。</td>
-</tr>
-<tr>
-<th>サンプル見出し</th>
-<td>サンプルテキスト。</td>
-</tr>
-<tr>
-<th>サンプル見出し</th>
-<td>サンプルテキスト。</td>
-</tr>
-</table>
+    <!-- モーダルの設定 -->
+    <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalScrollableTitle">Aさん</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h2>aaa</h2>
+            <p>fugafuga</p>
+            <p>fugafuga</p>
+            <p>fugafuga</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+            <button type="button" class="btn btn-primary">変更を保存</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
 </article>
 
-<p><a href="/javascript:history.back()">&lt;&lt; 前のページに戻る</a></p>
 
 </div>
 <!--/#contents-->
 
-</div>
-<!--/#container-->
+<script src="js/tutor_modal.js" type="text/javascript"></script>
 
 @endsection
+
